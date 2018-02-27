6248aeea506c63205a4cf3667890205b800d4f60
Amrani Houssain <amranidev@gmail.com>
2018-02-27 21:40:43 +0000

    update matrix.test.ts

--- __tests__/matrix.test.ts
+++ __tests__/matrix.test.ts
@@ -1,56 +1,10 @@
 import { MathHelpers } from "./../src/math/index"
 
-import { Cervello }  from "./../dist/cervello"
-
-/**
- * @todo Complete the tests
- */
-describe("Nerual Network Testing", () => {
-
-  let options = {
-    iteration: 10000,
-    hiddenLayer: 1,
-    units: 3
-  }
-
-  let mind = new Cervello.NeuralNetwork(options)
-      .train([
-        { input: [0, 0], output: [0] },
-        { input: [0, 1], output: [1] },
-        { input: [1, 0], output: [1] },
-        { input: [1, 1], output: [0] }
-      ])
-
-      test('Predicted value should be between 0 and 1', () => {
-
-        var result = mind.predict([0,1])[0][0]
-
-        expect(result).toBeGreaterThan(0)
-
-        expect(result).toBeLessThanOrEqual(1)
-
-      })
-})
-
-/**
- * @todo Complete the tests
- */
-describe("Linear Regression Testing", () => {
-    let data = {inputs: [1,2,3,4,5,6], outputs: [1,2,3,4,5,6]}
-        const lr = new Cervello.LinearRegression()
-        const lr2 = lr.train(data)
-        test("Predicted value should be 7", () => {
-            expect(lr.predict(7)).toBeGreaterThan(6.9)
-            expect(lr.predict(7)).toBeLessThanOrEqual(7)
-        })
-})
-
-
 describe("MathHelpers Testing", () => {
-    let matrix = new MathHelpers.Matrix([[1,2,3]])
-    let tmp: MathHelpers.Matrix
-    test("Transpose Matrix", () => {
-      tmp = matrix.transpose()
-      expect(tmp.m).toEqual([[1], [2], [3]])
-    })
+  let matrix = new MathHelpers.Matrix([[1, 2, 3]])
+  let tmp: MathHelpers.Matrix
+  test("Transpose Matrix", () => {
+    tmp = matrix.transpose()
+    expect(tmp.m).toEqual([[1], [2], [3]])
+  })
 })
